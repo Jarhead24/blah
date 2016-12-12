c007765e1c52a48c84309fb452be948c00cbbd38
canpan14 <sammachlin@hotmail.com>
2016-12-12 17:34:48 -0500

    Refactored the board class and moved and amount into the room class.
    Change dungeon generation so rooms are generated with at least a wall in between each others (before halls are carved out).

--- game/Game/Game/Board.cs
+++ game/Game/Game/Board.cs
@@ -10,7 +10,7 @@ namespace Game
     class Board
     {
         private static Board instance = null;
-        private tiles[,] board;
+        public tiles[,] board;
         private Random rand = null;
 
         public static int height;
@@ -86,14 +86,6 @@ namespace Game
         private void createRoom()
         {
             Room newRoom = new Room();
-            newRoom = defineCenterOfRoom(newRoom);
-            
-            // How far out in each direction it goes. Not the actual height/width
-            // -2 is because -1 to stay 1 away from edge and -1 because array ends at -1 width/height
-            int roomHeight = rand.Next(1, getValidHeightOfRoom(newRoom));
-            int roomWidth = rand.Next(1, getValidWidthOfRoom(newRoom));
-            newRoom.distanceUp = roomHeight;
-            newRoom.distanceRight = roomWidth;
             drawRoom(newRoom);
             roomsCreated.Add(newRoom);
         }
@@ -111,122 +103,6 @@ namespace Game
             player.yLocation = randomY;
             player.CurrentTileOn = board[randomY, randomX];
             board[randomY, randomX] = player.PlayerTile;
-        }
-
-        /// <summary>
-        /// Finds a valid spot for the center of a new room.
-        /// </summary>
-        /// <param name="room"></param>
-        /// <returns>Returns a room now defined with a valid center.</returns>
-        private Room defineCenterOfRoom(Room room)
-        {
-            
-            bool isCenterValid = false;
-            int randHeight = 0;
-            int randWidth = 0;
-            long startTime = Environment.TickCount;
-            // -1 because index. -2 to keep away from board border. +1 because of the way rand works
-            while (!isCenterValid && Environment.TickCount - startTime < 5000)
-            {
-                randHeight = rand.Next(2, (height - 1) - 2 + 1);
-                randWidth = rand.Next(2, (width - 1) - 2 + 1);
-                isCenterValid = checkForValidCenter(randWidth, randHeight);
-            }
-            if (Environment.TickCount - startTime >= 5000)
-            {
-                Console.WriteLine("Ran out of space placing rooms. Try making a bigger board or creating less rooms.");
-                Console.WriteLine("Press Enter to exit.");
-                Console.ReadLine();
-                Environment.Exit(0);
-            }
-            room.yCenter = randHeight;
-            room.xCenter = randWidth;
-            return room;
-        }
-
-        /// <summary>
-        /// Checks if a given coordinate is a valid location for the center of a new room.
-        /// </summary>
-        /// <param name="x">x location</param>
-        /// <param name="y">y location</param>
-        /// <returns>true if it's a valid center</returns>
-        private bool checkForValidCenter(int x, int y)
-        {
-            bool isValid = true;
-            for (int j = -1; j <= 1; j++)
-            {
-                for (int k = -1; k <= 1; k++)
-                {
-                    if (board[y + j, x + k].Symbol != WALL_TILE.Symbol )
-                    {
-                        isValid = false;
-                    }
-                }
-            }
-
-            return isValid;
-        }
-
-        /// <summary>
-        /// Gets the maximum width of a room
-        /// </summary>
-        /// <param name="room">room with already defined center</param>
-        /// <returns>maxiumum room width</returns>
-        private int getValidWidthOfRoom(Room room)
-        {
-            int xCenter = room.xCenter;
-            int maxRight = width - xCenter - 2;
-            int maxLeft = xCenter - 1;
-            // Check max to the right
-            for (int i = xCenter + 2; i < width; i++)
-            {
-                if (board[room.yCenter, i].Symbol != WALL_TILE.Symbol)
-                {
-                    maxRight = Math.Min(maxRight, Math.Abs(xCenter - (i - 1)));
-                    break;
-                }
-            }
-            // Check max to the left
-            for (int i = xCenter - 2; i > 0; i--)
-            {
-                if (board[room.yCenter, i].Symbol != WALL_TILE.Symbol)
-                {
-                    maxLeft = Math.Min(maxLeft, Math.Abs(xCenter - (i + 1)));
-                    break;
-                }
-            }
-            return Math.Min(maxLeft, maxRight);
-        }
-
-        /// <summary>
-        /// Gets the maxium height of a room
-        /// </summary>
-        /// <param name="room">room with already defined center</param>
-        /// <returns>maximum room height</returns>
-        private int getValidHeightOfRoom(Room room)
-        {
-            int yCenter = room.yCenter;
-            int maxUp = yCenter - 1;
-            int maxDown = height - yCenter - 2;
-            // Check max down
-            for (int i = yCenter + 2; i < height; i++)
-            {
-                if (board[i, room.xCenter].Symbol != WALL_TILE.Symbol)
-                {
-                    maxDown = Math.Min(maxDown, Math.Abs(yCenter - (i - 1)));
-                    break;
-                }
-            }
-            // Check max up
-            for (int i = yCenter - 2; i > 0; i--)
-            {
-                if (board[i, room.xCenter].Symbol != WALL_TILE.Symbol)
-                {
-                    maxUp = Math.Min(maxUp, Math.Abs(yCenter - (i + 1)));
-                    break;
-                }
-            }
-            return Math.Min(maxUp, maxDown);
         }
 
         /// <summary>
--- game/Game/Game/Room.cs
+++ game/Game/Game/Room.cs
@@ -6,11 +6,159 @@ using System.Threading.Tasks;
 
 namespace Game
 {
+
     class Room
     {
+        private Board boardClass = Board.Instance;
+        private Random rand = new Random(Environment.TickCount);
+        private int height = Board.height;
+        private int width = Board.width;
         public int xCenter { get; set; }
         public int yCenter { get; set; }
         public int distanceUp { get; set; }
         public int distanceRight { get; set; }
+
+        public Room()
+        {
+            defineCenterOfRoom();
+            int roomHeight = 0;
+            int roomWidth = 0;
+            // randomly decides height or width priority for room creation
+            int hOrWPriority = rand.Next(0, 2);
+            if(hOrWPriority == 0)
+            {
+                roomHeight = rand.Next(1, getValidHeightOfRoom(1));
+                roomWidth = rand.Next(1, getValidWidthOfRoom(roomHeight));
+            }
+            else
+            {
+                roomWidth = rand.Next(1, getValidWidthOfRoom(1));
+                roomHeight = rand.Next(1, getValidHeightOfRoom(roomWidth));
+            }
+            this.distanceUp = roomHeight;
+            this.distanceRight = roomWidth;
+        }
+
+        /// <summary>
+        /// Finds a valid spot for the center of a new room.
+        /// </summary>
+        public void defineCenterOfRoom()
+        {
+            bool isCenterValid = false;
+            int randHeight = 0;
+            int randWidth = 0;
+            long startTime = Environment.TickCount;
+            // -1 because index. -2 to keep away from board border. +1 because of the way rand works
+            while (!isCenterValid && Environment.TickCount - startTime < 5000)
+            {
+                randHeight = rand.Next(2, (height - 1) - 2 + 1);
+                randWidth = rand.Next(2, (width - 1) - 2 + 1);
+                isCenterValid = checkForValidCenter(randWidth, randHeight);
+            }
+            if (Environment.TickCount - startTime >= 5000)
+            {
+                Console.WriteLine("Ran out of space placing rooms. Try making a bigger board or creating less rooms.");
+                Console.WriteLine("Press Enter to exit.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            this.yCenter = randHeight;
+            this.xCenter = randWidth;
+        }
+
+        /// <summary>
+        /// Checks if a given coordinate is a valid location for the center of a new room.
+        /// </summary>
+        /// <param name="x">x location</param>
+        /// <param name="y">y location</param>
+        /// <returns>true if it's a valid center</returns>
+        private bool checkForValidCenter(int x, int y)
+        {
+            bool isValid = true;
+            for (int j = -2; j <= 2; j++)
+            {
+                for (int k = -2; k <= 2; k++)
+                {
+                    if (boardClass.board[y + j, x + k].Symbol != Board.WALL_TILE.Symbol)
+                    {
+                        isValid = false;
+                    }
+                }
+            }
+
+            return isValid;
+        }
+
+        /// <summary>
+        /// Gets the maximum width of a room
+        /// </summary>
+        /// <param name="roomHeight">height of room</param>
+        /// <returns>maxiumum room width</returns>
+        private int getValidWidthOfRoom(int roomHeight)
+        {
+            int maxRight = width - xCenter - 2;
+            int maxLeft = xCenter - 1;
+            // Check max to the right
+            for (int i = xCenter + 2; i < width; i++)
+            {
+                for (int j = -roomHeight; j <= roomHeight; j++)
+                {
+                    if (boardClass.board[j + yCenter, i].Symbol != Board.WALL_TILE.Symbol)
+                    {
+                        maxRight = Math.Min(maxRight, Math.Abs(xCenter - (i - 1)));
+                        break;
+                    }
+                }
+            }
+            // Check max to the left
+            for (int i = xCenter - 2; i > 0; i--)
+            {
+                for (int j = -roomHeight; j <= roomHeight; j++)
+                {
+                    if (boardClass.board[j + yCenter, i].Symbol != Board.WALL_TILE.Symbol)
+                    {
+                        maxLeft = Math.Min(maxLeft, Math.Abs(xCenter - (i + 1)));
+                        break;
+                    }
+                }
+            }
+            return Math.Min(maxLeft, maxRight);
+        }
+
+        /// <summary>
+        /// Gets the maxium height of a room
+        /// </summary>
+        /// <param name="roomWidth">current width of room</param>
+        /// <returns>maximum room height</returns>
+        private int getValidHeightOfRoom(int roomWidth)
+        {
+            int maxUp = yCenter - 1;
+            int maxDown = height - yCenter - 2;
+            // Check max down
+            for (int i = yCenter + 2; i < height; i++)
+            {
+                for (int j = -roomWidth; j <= roomWidth; j++)
+                {
+                    if (boardClass.board[i, j + xCenter].Symbol != Board.WALL_TILE.Symbol)
+                    {
+                        maxDown = Math.Min(maxDown, Math.Abs(yCenter - (i - 1)));
+                        break;
+                    }
+                }
+            }
+            // Check max up
+            for (int i = yCenter - 2; i > 0; i--)
+            {
+                for (int j = -roomWidth; j <= roomWidth; j++)
+                {
+                    if (boardClass.board[i, j + xCenter].Symbol != Board.WALL_TILE.Symbol)
+                    {
+                        maxUp = Math.Min(maxUp, Math.Abs(yCenter - (i + 1)));
+                        break;
+                    }
+                }
+            }
+            return Math.Min(maxUp, maxDown);
+        }
     }
 }
