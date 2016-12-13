2367aeac047db3605e3679536e1e70f094c12beb
canpan14 <sammachlin@hotmail.com>
2016-12-13 16:19:33 -0500

    DieRoller wasn't added for some reason.

---
+++ game/Game/Game/DieRoller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class DieRoller
+    {
+        public static int roll()
+        {
+            return StaticRandom.Instance.Next(1, 7);
+        }
+        public static int roll(int sides)
+        {
+            return StaticRandom.Instance.Next(1, sides + 1);
+        }
+        public static int roll(int numd, int sides)
+        {
+            int total = 0;
+            for (int i = 0; i < numd; i++)
+            {
+                total += StaticRandom.Instance.Next(1, sides + 1);
+            }
+            return total;
+
+        }
+        public static int roll(int numd, int sides, int t)
+        {
+            int x = 0;
+
+            for (int i = 0; i < numd; i++)
+            {
+                if (StaticRandom.Instance.Next(1, sides + 1) >= t)
+                { x++; }
+            }
+            return x;
+        }
+    }
+}
